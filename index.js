da4bd9737dc05bb20a69c2c890afcaf736d28e57
Dillon Murphy <Dmurph21@me.com>
2017-04-16 11:01:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -19,21 +19,7 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://rpsavas.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  },
-    push: {
-  	ios: [
-    	{
-      		pfx: '/Users/Murph/Desktop/Extra\ Files/Certificates\ and\ Profiles/RPSavvyDevParse.p12’, // Dev PFX or P12
-      		bundleId: ‘com.Strategyn-Mobile-Pros.RPSavvy’,
-      		production: false // Dev
-    	},
-    	{
-      		pfx: '/Users/Murph/Desktop/Extra\ Files/Certificates\ and\ Profiles/RPSavvyProdParse.p12', // Prod PFX or P12
-      		bundleId: ‘com.Strategyn-Mobile-Pros.RPSavvy’,  
-      		production: true // Prod
-    }
-  ]
- }
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
